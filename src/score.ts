336bcef7cdc9c94bcbd387874d482a62de4c0fdb
petetheyeet <peter@team7.tech>
2021-03-11 06:09:14 -0500

    Board changes. import board state

---
+++ src/score.ts
@@ -0,0 +1,24 @@
+
+
+export interface Score {
+
+    id: string;
+    name: string;
+    score: number;
+
+};
+
+export const SCORES: Score[] = [
+    { id: "1", name: "Spencer Church", score: 24 },
+    { id: "1", name: "Adele Gonzalez", score: 85 },
+    { id: "1", name: "Jasper Mueller", score: 35 },
+    { id: "1", name: "Beth Bray", score: 75 },
+    { id: "1", name: "Nate Daniels", score: 64 },
+    { id: "1", name: "Lilly Henry", score: 71 },
+    { id: "1", name: "Zach Dearborn", score: 79 },
+    { id: "1", name: "Yasmine Chapman", score: 30 },
+    { id: "1", name: "Spike Spiegel", score: 80 },
+    { id: "1", name: "Anya Vaughn", score: 40 },
+    { id: "1", name: "Juan Hensley", score: 25 },
+    { id: "1", name: "Stacey Calhoun", score: 27 },
+];
